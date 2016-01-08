8845083f20487c4ca82105d651aed35f0d765f42
lkjy <951147663@qq.com>
2016-01-08 16:26:29 +0800

    第一次提交，哈哈哈

---
+++ 245/245/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  245
+//
+//  Created by lkjy on 16/1/8.
+//  Copyright © 2016年 lkjy. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
